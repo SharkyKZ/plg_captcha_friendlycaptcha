ff8032732dba17299f9f94c0016e887c52ddc38d
SharkyKZ <sharkykz@gmail.com>
2022-10-12 10:18:24 +0300

    Build script improvements

--- build/build.php
+++ build/build.php
@@ -1,45 +1,166 @@
 <?php
-// @todo proper build tools
 
 define('PATH_ROOT', str_replace('\\', '/', dirname(__DIR__)));
 
-if (!is_dir(__DIR__ . '/zips'))
+final class PluginBuildScript
 {
-	mkdir(__DIR__ . '/zips', 0755);
-}
+	private const PLUGIN_ELEMENT = 'friendlycaptcha';
+	private const PLUGIN_TYPE = 'captcha';
 
-$manifest = simplexml_load_file(PATH_ROOT . '/code/plugins/captcha/friendlycaptcha/friendlycaptcha.xml');
-$version = (string) $manifest->children()->version;
+	private const JOOMLA_MINIMUM = '3.8';
+	private const JOOMLA_UNSUPPORTED = '5.0';
+	private const PHP_MINIMUM = '5.3.10';
+	private const PHP_UNSUPPORTED = '8.3';
 
-$zip = new ZipArchive;
-$zipFile = __DIR__ . '/zips/plg_captcha_friendlycaptcha-' . $version . '.zip';
-$zip->open($zipFile, ZipArchive::CREATE);
-$directories = [PATH_ROOT . '/code/plugins/captcha/friendlycaptcha', PATH_ROOT . '/code/media/plg_captcha_friendlycaptcha'];
+	private const UPDATE_JOOMLA_REGEX = '(4|3\.([89]|10))';
+	private const UPDATE_NAME = 'Captcha - Friendly Captcha';
+	private const UPDATE_DESCRIPTION = 'Friendly Captcha anti-spam plugin.';
+	private const REPOSITORY_NAME = 'plg_captcha_friendlycaptcha';
 
-foreach ($directories as $directory)
-{
-	$iterator = new RecursiveDirectoryIterator($directory);
-	$iterator2 = new RecursiveIteratorIterator($iterator);
+	private string $pluginDirectory;
+	private string $mediaDirectory;
+	private string $pluginName;
+	private string $repositoryUrl;
+	private string $zipFile;
+	private string $version;
 
-	foreach ($iterator2 as $file)
+	public function __construct()
 	{
-		if ($file->isFile())
+		$this->pluginName = 'plg_' . self::PLUGIN_TYPE . '_' . self::PLUGIN_ELEMENT;
+
+		$this->pluginDirectory = PATH_ROOT . '/code/plugins/' . self::PLUGIN_TYPE . '/' . self::PLUGIN_ELEMENT;
+		$this->mediaDirectory = PATH_ROOT . '/code/media/' . $this->pluginName;
+
+		$xml = simplexml_load_file($this->pluginDirectory . '/' . self::PLUGIN_ELEMENT . '.xml');
+		$this->version = (string) $xml->version;
+
+		$this->repositoryUrl = 'https://github.com/SharkyKZ/' . self::REPOSITORY_NAME;
+		$this->zipFile = __DIR__ . '/zips/' . $this->pluginName . '-' . $this->version . '.zip';
+	}
+
+	public function build(): void
+	{
+		$this->buildZip();
+		$this->updateUpdateXml();
+		$this->updateChangelogXml();
+	}
+
+	private function buildZip(): void
+	{
+		if (!is_dir(__DIR__ . '/zips'))
 		{
-			$zip->addFile(
-				$file->getPathName(),
-				str_replace(['\\', PATH_ROOT . '/code/', 'plugins/captcha/friendlycaptcha/', 'media/plg_captcha_friendlycaptcha/'], ['/', '', '', 'media/'], $file->getPathName())
-			);
+			mkdir(__DIR__ . '/zips', 0755);
 		}
+
+		$zip = new ZipArchive;
+		$zip->open($this->zipFile, ZipArchive::OVERWRITE|ZipArchive::CREATE);
+		$directories = [$this->pluginDirectory, $this->mediaDirectory];
+
+		foreach ($directories as $directory)
+		{
+			$iterator = new RecursiveDirectoryIterator($directory);
+			$iterator2 = new RecursiveIteratorIterator($iterator);
+
+			foreach ($iterator2 as $file)
+			{
+				if ($file->isFile())
+				{
+					$zip->addFile(
+						$file->getPathName(),
+						str_replace(['\\', $this->pluginDirectory . '/', $this->mediaDirectory . '/'], ['/', '', 'media/'], $file->getPathName())
+					);
+				}
+			}
+		}
+
+		//$zip->addFile(PATH_ROOT . '/LICENSE', 'LICENSE');
+		$zip->close();
+	}
+
+	private function updateUpdateXml(): void
+	{
+		$manifestFile = PATH_ROOT . '/updates/updates.xml';
+		$xml = simplexml_load_file($manifestFile);
+		$children = $xml->xpath('update');
+
+		foreach ($children as $key => $update)
+		{
+			if (
+				(string) $update->version === $this->version
+				|| ((string) $update->targetplatform->attributes()['version'] === self::UPDATE_JOOMLA_REGEX && (string) $update->php_minimum === self::PHP_MINIMUM)
+			)
+			{
+				unset($update[0]);
+			}
+		}
+
+		//  Static values.
+		$update = $xml->addChild('update');
+		$update->addChild('name', self::UPDATE_NAME);
+		$update->addChild('description', self::UPDATE_DESCRIPTION);
+		$update->addChild('element', self::PLUGIN_ELEMENT);
+		$update->addChild('type', 'plugin');
+		$update->addChild('folder', self::PLUGIN_TYPE);
+		$update->addChild('client', 'site');
+		$update->addChild('maintainer', 'SharkyKZ');
+		$update->addChild('maintainerurl', $this->repositoryUrl);
+
+		// Dynamic values.
+		$update->addChild('version', $this->version);
+		$node = $update->addChild('downloads');
+		$node = $node->addChild('downloadurl', $this->repositoryUrl . '/releases/download/' . $this->version . '/' . basename($this->zipFile));
+		$node->addAttribute('type', 'full');
+		$node->addAttribute('format', 'zip');
+
+		foreach (array_intersect(['sha512', 'sha384', 'sha256'], hash_algos()) as $algo)
+		{
+			$update->addChild($algo, hash_file($algo, $this->zipFile));
+		}
+
+		$node = $update->addChild('infourl', $this->repositoryUrl . '/releases/tag/' . $this->version);
+		$node->addAttribute('title', self::UPDATE_NAME);
+		$update->addChild('changelogurl', 'https://raw.githubusercontent.com/SharkyKZ/' . self::REPOSITORY_NAME . '/master/updates/changelog.xml');
+
+		// System requirements.
+		$node = $update->addChild('targetplatform');
+		$node->addAttribute('name', 'joomla');
+		$node->addAttribute('version', self::UPDATE_JOOMLA_REGEX);
+		$update->addChild('php_minimum', self::PHP_MINIMUM);
+
+		file_put_contents($manifestFile, $this->formatXml($xml->asXml()));
+	}
+
+	private function updateChangelogXml(): void
+	{
+		$manifestFile = PATH_ROOT . '/updates/changelog.xml';
+		$xml = simplexml_load_file($manifestFile);
+
+		foreach ($xml->children() as $update)
+		{
+			if ((string) $update->version === $this->version)
+			{
+				return;
+			}
+		}
+
+		$changelog = $xml->addChild('changelog');
+		$changelog->addChild('element', self::PLUGIN_ELEMENT);
+		$changelog->addChild('type', 'plugin');
+		$changelog->addChild('version', $this->version);
+
+		file_put_contents($manifestFile, $this->formatXml($xml->asXml()));
+	}
+
+	private function formatXml(string $xml): string
+	{
+		$dom = new DOMDocument;
+		$dom->preserveWhiteSpace = false;
+		$dom->formatOutput = true;
+		$dom->loadXml($xml);
+		$output = $dom->saveXML();
+
+		return str_replace('  ', "\t", $output);
 	}
 }
 
-$zip->close();
-
-$hashes = '';
-
-foreach (array_intersect(['sha512', 'sha384', 'sha256'], hash_algos()) as $algo)
-{
-	$hashes .= '<' . $algo . '>' . hash_file($algo, $zipFile) . '</' . $algo . ">\n";
-}
-
-file_put_contents(__DIR__ . '/zips/hashes.xml', $hashes);
+(new PluginBuildScript)->build();
--- build/setup.php
+++ build/setup.php
@@ -27,7 +27,25 @@ $hashes = [];
 foreach ($mediaFiles as $file)
 {
 	copy(PATH_ROOT .  '/node_modules/friendly-challenge/' . $file, $mediaPath . '/' . $file);
-	$hashes[] = $file . ': ' . base64_encode(hash_file('sha384', $mediaPath . '/' . $file, true));
+	$hashes[$file] = base64_encode(hash_file('sha384', $mediaPath . '/' . $file, true));
 }
 
-file_put_contents(__DIR__ . '/zips/sri.txt', implode("\n", $hashes));
+$filename = PATH_ROOT . '/code/plugins/captcha/friendlycaptcha/friendlycaptcha.php';
+$code = file_get_contents($filename);
+$pattern = '/\'(widget.*\.js)\'\s+=>\s+\'sha384\-(.*)\'/';
+
+$code = preg_replace_callback(
+	$pattern,
+	static function ($match) use ($hashes)
+	{
+		return str_replace($match[2], $hashes[$match[1]], $match[0]);
+	},
+	$code
+);
+
+$json = json_decode(file_get_contents(PATH_ROOT . '/node_modules/friendly-challenge/package.json'));
+$pattern = '/(const\s+CHALLENGE_VERSION\s+=\s+\')(.*)(\';)/';
+preg_match($pattern, $code, $matches);
+$code = preg_replace($pattern, '${1}' . $json->version . '$3', $code);
+
+file_put_contents($filename, $code);
